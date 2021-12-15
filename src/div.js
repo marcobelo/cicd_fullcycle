179bafe45101691be9394d7d4eecfcfdfeaaf583
Marco Belo <marco.barone.belo@gmail.com>
2021-12-15 14:35:51 -0300

    testing quality gate2

--- src/div.js
+++ src/div.js
@@ -2,5 +2,17 @@ function div(a, b) {
     return a / b;
 }
 
+function div2(a, b) {
+    return a / b;
+}
+
+function div3(a, b) {
+    return a / b;
+}
+
+function div4(a, b) {
+    return a / b;
+}
+
 
 module.exports = div;
